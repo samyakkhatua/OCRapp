9df47d8f0061e6227afb6dac90442c25e675f00a
samyakkhatua <samyakkhatua@duck.com>
2022-11-21 20:30:19 +0530

    basic UI done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
-// import { useState } from 'react'
+import { useState } from "react";
 // import './App.css'
 
 function App() {
+  // const [loading, setLoading] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [text, setText] = useState("");
+  const [image, setImage] = useState("");
+
+  const handleSubmit = () => {};
 
   return (
-    <div className="">
-      <h1 className="text-3xl font-bold underline">
+    <>
+      <div className="container">
+        <p className="text-3xl font-bold text-center mb-20">OCR app</p>
+        <input
+          type="file"
+          onChange={(e) => setImage(URL.createObjectURL(e.target.files[0]))}
+        />
+        <input
+          type="button"
+          onChange={handleSubmit}
+          className="bg-black text-white p-2 rounded-md"
+          value="Convert"
+        />
 
-
-      Samyak OCR app which can scan & extract text on a image.
-      </h1>
-    </div>
-  )
+        <div className="flex border-black border-2 mt-10 items-stretch w-[50%]">
+          <textarea
+            cols="50"
+            rows="10"
+            className=""
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          ></textarea>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
