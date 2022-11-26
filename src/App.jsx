dddc5c432a874d30ad4ba58939bd9cdcb2a5dfc4
samyakkhatua <samyakkhatua@duck.com>
2022-11-26 23:50:00 +0530

    mode toggle added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Tesseract from "tesseract.js";
 function App() {
   const [text, setText] = useState("");
   const [image, setImage] = useState("");
+  const [mode, setMode] = useState("capture");
 
   const handleSubmit = () => {
     Tesseract.recognize(image, "eng", {
@@ -21,8 +22,37 @@ function App() {
       });
   };
 
+  const handleMode = () => {
+    setMode("upload");
+  };
+
   return (
     <>
+      {/* container  */}
+      <div>
+        {/* upload capture toggle  */}
+        <div>
+          <label class="inline-flex relative items-center cursor-pointer">
+            <input
+              type="checkbox"
+              value=""
+              placeholder=""
+              class="sr-only peer"
+            />
+
+            <div
+              class="w-14 h-7 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[4px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-6 after:w-6 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"
+              onClick={handleMode}
+            ></div>
+
+            <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+              {mode}
+            </span>
+          </label>
+        </div>
+      </div>
+
+      {/* ==================================================================== */}
       <div className="container">
         <p className="text-3xl font-bold text-center mb-20">OCR app</p>
         <input
